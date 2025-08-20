ead26f99806470d6e8b587b586113db6e687ba20
Chase Carter <149976513+Theinfection91@users.noreply.github.com>
2025-08-20 18:34:01 -0400

    Changing Set commands to Settings commands and will have another nested class inside for another group of slash commands like ladderbot does to help with ease of typing and organizing.

---
+++ src/FlawsFightNight.Bot/SlashCommands/SettingsCommands.cs
@@ -0,0 +1,62 @@
+﻿using Discord;
+using Discord.Interactions;
+using FlawsFightNight.CommandsLogic.SetCommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlawsFightNight.Bot.SlashCommands
+{
+    [Group("settings", "Commands for tournament and admin settings")]
+    public class SettingsCommands : InteractionModuleBase<SocketInteractionContext>
+    {
+        [Group("matches_channel_id", "Set or remove the channel ID for matches of a specified tournament")]
+        public class MatchesChannelCommands : InteractionModuleBase<SocketInteractionContext>
+        {
+            private SetMatchesChannelLogic _setMatchesChannelLogic;
+            public MatchesChannelCommands(SetMatchesChannelLogic setMatchesChannelLogic)
+            {
+                _setMatchesChannelLogic = setMatchesChannelLogic;
+            }
+
+            [SlashCommand("set", "Set the channel ID for matches of a specified tournament")]
+            [Discord.Commands.RequireUserPermission(GuildPermission.Administrator)]
+            public async Task SetMatchesChannelIdAsync(
+            [Summary("tournament_id", "The ID of the tournament to set the matches channel for")] string tournamentId,
+            [Summary("channel_id", "The ID of the channel where matches will be posted")] IMessageChannel channel)
+            {
+                try
+                {
+                    var result = _setMatchesChannelLogic.SetMatchesChannelProcess(tournamentId, channel);
+                    await RespondAsync(embed: result);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Command Error: {ex}");
+                    await RespondAsync("An error occurred while processing this command.", ephemeral: true);
+                }
+            }
+
+            [SlashCommand("remove", "Remove the channel ID for matches of a specified tournament")]
+            [Discord.Commands.RequireUserPermission(GuildPermission.Administrator)]
+            public async Task SetMatchesChannelIdAsync(
+            [Summary("tournament_id", "The ID of the tournament to set the matches channel for")] string tournamentId)
+            {
+                try
+                {
+                    // TODO: Implement the logic to remove the matches channel ID
+
+                    //var result = ;
+                    //await RespondAsync(embed: result);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Command Error: {ex}");
+                    await RespondAsync("An error occurred while processing this command.", ephemeral: true);
+                }
+            }
+        }
+    }
+}
