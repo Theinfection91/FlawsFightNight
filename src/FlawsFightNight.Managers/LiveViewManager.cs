a373efd3d6c45b362738bed8e45fbb5808fcbca0
Chase Carter <149976513+Theinfection91@users.noreply.github.com>
2025-09-04 07:08:52 -0400

    Ran bot all night, standings task stopped sending messages around 4:17am while other tasks were still working. No errors, task keeps silently failing. May just rewrite entire standings task process.

--- src/FlawsFightNight.Managers/LiveViewManager.cs
+++ src/FlawsFightNight.Managers/LiveViewManager.cs
@@ -202,6 +202,7 @@ namespace FlawsFightNight.Managers
 
         public void StartStandingsLiveViewTask()
         {
+            // TODO Figure out why Standings Task silently fails after some time, other tasks work fine
             Task.Run(() => RunStandingsUpdateTaskAsync());
         }
 
