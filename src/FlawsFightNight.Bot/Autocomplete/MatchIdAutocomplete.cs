c94cff5de06bf7ce1d60e01fed791dc90491ec98
Chase Carter <149976513+Theinfection91@users.noreply.github.com>
2025-10-22 18:00:10 -0400

    Created MatchIdAutocomplete. Still needs logic.

---
+++ src/FlawsFightNight.Bot/Autocomplete/MatchIdAutocomplete.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlawsFightNight.Bot.Autocomplete
+{
+    public class MatchIdAutocomplete : AutocompleteHandler
+    {
+
+    }
+}
