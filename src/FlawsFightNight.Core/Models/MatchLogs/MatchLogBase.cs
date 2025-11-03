b4442a4175c002de47306b6fb9d041e07f67bcbd
Chase Carter <149976513+Theinfection91@users.noreply.github.com>
2025-11-03 13:01:25 -0500

    Gonna create all this parallel to whats already here. Got a tournament base with a matchlog base that inherits an IMatchLog now

---
+++ src/FlawsFightNight.Core/Models/MatchLogs/MatchLogBase.cs
@@ -0,0 +1,17 @@
+﻿using FlawsFightNight.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlawsFightNight.Core.Models.MatchLogs
+{
+    public abstract class MatchLogBase : IMatchLog
+    {
+        public abstract List<Match> GetAllActiveMatches(int currentRound = 0);
+        public abstract List<PostMatch> GetAllPostMatches();
+        public virtual (int pointsFor, int pointsAgainst) GetPointsForAndAgainst(string teamName)
+        => (0, 0);
+    }
+}
---
+++ src/FlawsFightNight.Core/Models/Tournaments/TournamentBase.cs
@@ -0,0 +1,29 @@
+﻿using FlawsFightNight.Core.Enums;
+using FlawsFightNight.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlawsFightNight.Core.Models.Tournaments
+{
+    public abstract class TournamentBase
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public TournamentType Type { get; protected set; }
+        public int TeamSize { get; set; }
+        public string TeamSizeFormat => $"{TeamSize}v{TeamSize}";
+        public List<Team> Teams { get; set; }
+        public bool IsRunning { get; set; } = false;
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+        public IMatchLog MatchLog { get; protected set; }
+
+        public abstract void Start();
+        public abstract void End();
+        public abstract string GetFormattedType();
+    }
+}
