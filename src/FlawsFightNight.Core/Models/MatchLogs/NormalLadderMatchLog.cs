815b0eb6f6ce4f82e06affccfde8c126cf1534b3
Chase Carter <149976513+Theinfection91@users.noreply.github.com>
2025-11-04 15:43:06 -0500

    Added NormalLadderTournament and NormalLadderMatchLog

---
+++ src/FlawsFightNight.Core/Models/MatchLogs/NormalLadderMatchLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlawsFightNight.Core.Models.MatchLogs
+{
+    public class NormalLadderMatchLog : MatchLogBase
+    {
+        public List<Match> MatchesToPlay { get; set; } = new();
+        public List<PostMatch> PostMatches { get; set; } = new();
+
+        public NormalLadderMatchLog() { }
+
+        public override List<Match> GetAllActiveMatches(int currentRound = 0) => MatchesToPlay;
+        public override List<PostMatch> GetAllPostMatches() => PostMatches;
+    }
+}
---
+++ src/FlawsFightNight.Core/Models/Tournaments/NormalLadderTournament.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlawsFightNight.Core.Enums;
+using FlawsFightNight.Core.Models.MatchLogs;
+
+namespace FlawsFightNight.Core.Models.Tournaments
+{
+    public class NormalLadderTournament : TournamentBase // TODO IRankSystem
+    {
+        public NormalLadderTournament()
+        {
+            Type = TournamentType.Ladder;
+            MatchLog = new NormalLadderMatchLog();
+        }
+
+        public override void Start()
+        {
+            IsRunning = true;
+            // TODO Add Ladder specific start logic here
+        }
+
+        public override void End() => IsRunning = false;
+
+        public override string GetFormattedType() => "Normal Ladder";
+    }
+}
