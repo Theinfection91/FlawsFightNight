6c6dce5bf27c5b789cd6e96c9ca22aae172ee945
Chase Carter <149976513+Theinfection91@users.noreply.github.com>
2026-02-12 21:15:31 -0500

    More placeholder files but also now have IEncryptable in the Data project. Have it on FTPCredential now but will eventually want it on all credentials later.

--- src/FlawsFightNight.Data/DataModels/FTPCredential.cs
+++ src/FlawsFightNight.Data/DataModels/FTPCredential.cs
@@ -1,17 +1,36 @@
-﻿using System;
+﻿using FlawsFightNight.Data.Interfaces;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FlawsFightNight.Data.DataModels
 {
-    public class FTPCredential
+    public class FTPCredential : IEncryptable
     {
         public string? IPAddress { get; set; }
         public string? Username { get; set; }
-        public string? Password { get; set; }
-        public string? UserLogsDirectory {  get; set; }
+
+        private string? _encryptedPassword;
+
+        [JsonIgnore]
+        public string? Password
+        {
+            get => string.IsNullOrEmpty(_encryptedPassword) ? null : IEncryptable.Decrypt(_encryptedPassword);
+            set => _encryptedPassword = string.IsNullOrEmpty(value) ? null : IEncryptable.Encrypt(value);
+        }
+
+        // For JSON serialization
+        public string? EncryptedPassword
+        {
+            get => _encryptedPassword;
+            set => _encryptedPassword = value;
+        }
+
+        public string? UserLogsDirectory { get; set; }
 
         public FTPCredential() { }
     }
