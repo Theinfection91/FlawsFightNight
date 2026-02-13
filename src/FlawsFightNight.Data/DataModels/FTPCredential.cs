237e83e27451023973cb130821852710b3ae6070
Chase Carter <149976513+Theinfection91@users.noreply.github.com>
2026-02-12 19:22:06 -0500

    Adding models for FTPCredential and the FTPCrendentialFile that will hold all given creds. Really need to look into adding encryption on this one. Id hate for a hacked server to be my fault.

---
+++ src/FlawsFightNight.Data/DataModels/FTPCredential.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlawsFightNight.Data.DataModels
+{
+    public class FTPCredential
+    {
+        public string? IPAddress { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? UserLogsDirectory {  get; set; }
+
+        public FTPCredential() { }
+    }
+}
---
+++ src/FlawsFightNight.Data/DataModels/FTPCredentialFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlawsFightNight.Data.DataModels
+{
+    public class FTPCredentialFile
+    {
+        public List<FTPCredential> FTPCredentials { get; set; } = [];
+        public FTPCredentialFile() { }
+    }
+}
