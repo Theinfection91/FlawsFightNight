4b22058277ce10906bb60d77e459a1e9b65c2a8d
Chase Carter <149976513+Theinfection91@users.noreply.github.com>
2026-03-15 14:56:40 -0400

    Now have FTP service downloading files in memory then file streaming info instead of file stream inside ftp client.

--- src/FlawsFightNight.Commands/SettingsCommands/UT2004AdminCommands/TagLogToMatchHandler.cs
+++ src/FlawsFightNight.Commands/SettingsCommands/UT2004AdminCommands/TagLogToMatchHandler.cs
@@ -11,15 +11,20 @@ namespace FlawsFightNight.Commands.SettingsCommands.UT2004AdminCommands
     public class TagLogToMatchHandler : CommandHandler
     {
         public GitBackupService _gitBackupService;
+        public TournamentService _tournamentService;
+        public MatchService _matchService;
         public UT2004StatsService _ut2004StatsService;
-        public TagLogToMatchHandler(GitBackupService gitBackupService, UT2004StatsService ut2004StatsService) : base("Tag Log To Match")
+        public TagLogToMatchHandler(GitBackupService gitBackupService, TournamentService tournamentService, MatchService matchService, UT2004StatsService ut2004StatsService) : base("Tag Log To Match")
         {
             _gitBackupService = gitBackupService;
+            _tournamentService = tournamentService;
+            _matchService = matchService;
             _ut2004StatsService = ut2004StatsService;
         }
 
         public async Task<Embed> TagLogToMatchProcess(string statLogId, string tournamentId, string matchId)
         {
+
             return null;
         }
     }
